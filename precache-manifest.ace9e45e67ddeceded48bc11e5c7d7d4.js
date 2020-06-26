1977f4e37042b7364bdb6f08e02d200ffa1e55ff
jeeyeon <sdc9141@gmail.com>
2020-06-26 22:44:30 +0900

    Updates

---
+++ precache-manifest.ace9e45e67ddeceded48bc11e5c7d7d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26d6d24021921e6c0ff409ed79a74d4a",
+    "url": "/movie_react_app/index.html"
+  },
+  {
+    "revision": "5f4cef15de7b71827ba6",
+    "url": "/movie_react_app/static/css/main.4b9e28f8.chunk.css"
+  },
+  {
+    "revision": "c1b2f8091d8e08078211",
+    "url": "/movie_react_app/static/js/2.6b0abdec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_react_app/static/js/2.6b0abdec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f4cef15de7b71827ba6",
+    "url": "/movie_react_app/static/js/main.cd587383.chunk.js"
+  },
+  {
+    "revision": "ff546b76077b6510a32c",
+    "url": "/movie_react_app/static/js/runtime-main.7894784a.js"
+  }
+]);
