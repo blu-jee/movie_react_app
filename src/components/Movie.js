6fef296a11ec0ca8a1cc283fb3385d71f5293636
jeeyeon <sdc9141@gmail.com>
2020-07-01 12:07:15 +0900

    Add Genre Exception

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -4,8 +4,11 @@ import PropTypes from "prop-types";
 import "./Movie.css";
 
 function Movie({ id, year, title, summary, poster, genres }) {
+    if (!genres) {
+        console.log(title);
+        genres = [];
+    }
     return (
-
         <div className="movie">
             <Link to={{
                 pathname: `/movie/${id}`,
@@ -17,7 +20,13 @@ function Movie({ id, year, title, summary, poster, genres }) {
                 <div className="movie__data">
                     <h3 className="movie__title">{title}</h3>
                     <h5 className="movie__year">{year}</h5>
-                    <ul className="movie__genres">{genres.map((genre, index) => <li key={index} className="genres__genre">{genre}</li>)}</ul>
+                    <ul className="movie__genres">
+                        {genres.map((genre, index) => (
+                            <li key={index} className="genres__genre">
+                                {genre}
+                            </li>
+                        ))}
+                    </ul>
                     <p className="movie__summary">{summary.slice(0, 140)}...</p>
                 </div>
             </Link>
